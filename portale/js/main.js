4f269d69aa0e59ae570c0948a83bacf009545a52
ShinDarth <borzifrancesco@gmail.com>
2015-01-04 16:39:39 +0100

    First experiment calling WP API

--- portale/js/main.js
+++ portale/js/main.js
@@ -1,6 +1,22 @@
 (function () {
   var app = angular.module('portale', ['ui.bootstrap']);
 
-  // ...
+  app.controller("Controller", function($scope, $http) {
+
+    // indirizzo di wordpress con wp-json plugin
+    var wp = "/wordpress/wp-json/";
+
+    // chiamata a WP API
+    var call = "posts";
+
+    $http.get( wp + call )
+    .success(function(data, status, header, config) {
+      $scope.posts = data;
+    })
+    .error(function(data, status, header, config) {
+      alert("Errore");
+    });
+
+  });
 
 })()
