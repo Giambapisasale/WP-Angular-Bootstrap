355dbee18c9ed57a87ecfc8361f486050efc3450
Helias <stefano520@hotmail.it>
2015-01-11 13:10:48 +0100

    Added script to get menu data

--- portale/js/portale/menu.js
+++ portale/js/portale/menu.js
@@ -17,6 +17,23 @@
     .error(function(data, status, header, config) {
       console.log("Error in $http.get() of MenuController (pages)");
     });
-  });
 
+    $http.get( app.wp + "menus" )
+    .success(function(data, status, header, config) {
+      $scope.menu_list = data;      
+      $http.get( app.wp + "menus/" + $scope.menu_list[0].ID)
+        .success(function(data, status, header, config) {
+          $scope.menus = data;
+        })
+        .error(function(data, status, header, config) {
+          console.log("Error in $http.get() of MenuController (menu)");
+        });
+
+    })
+    .error(function(data, status, header, config) {
+      console.log("Error in $http.get() of MenuController (menus)");
+    });
+    
+    
+  });
 })()
