b3b8754b95da94d57cddd6cf292dd0e33cc05417
ShinDarth <borzifrancesco@gmail.com>
2015-01-19 15:54:07 +0100

    Brutto esempio

--- portale/js/portale/menu.js
+++ portale/js/portale/menu.js
@@ -18,31 +18,5 @@
       console.log("Error in $http.get() of MenuController (pages)");
     });
 
-    $http.get( app.wp + "menus" )
-    .success(function(data, status, header, config) {
-      $scope.menu_list = data;      
-      $http.get( app.wp + "menus/" + $scope.menu_list[0].ID)
-        .success(function(data, status, header, config) {
-          $scope.menus = data;
-          for(var i = 0; i < $scope.menus.items.length; i++)
-          {
-            var link = "";
-            var url = $scope.menus.items[i];
-            link += $scope.menus.items[i].object;
-            link += "/";
-            link += $scope.menus.items[i].object_id;
-            $scope.menus.items[i].url = link;
-          }
-        })
-        .error(function(data, status, header, config) {
-          console.log("Error in $http.get() of MenuController (menu)");
-        });
-
-    })
-    .error(function(data, status, header, config) {
-      console.log("Error in $http.get() of MenuController (menus)");
-    });
-    
-    
   });
 })()
