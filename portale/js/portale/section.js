47eac84a7b013a85986402788ec831b933c9b39b
Helias <stefano520@hotmail.it>
2015-02-06 20:29:12 +0100

    Aggiunta funzione Mostra/Nascondi sotto-menu

--- portale/js/portale/section.js
+++ portale/js/portale/section.js
@@ -1,7 +1,22 @@
+function show_submenu(menu_)
+{
+  var seconds = document.getElementsByClassName("second");
+  for (var i = 0; i < seconds.length; i++)
+  {
+    seconds[i].style.display = "none";
+  }
+  
+  var sub_menu = menu_.getElementsByClassName("second");
+  for (var i = 0; i < sub_menu.length; i++)
+  {
+    sub_menu[i].style.display = "block"; 
+  }
+}
+
 (function () {
   var app = angular.module('portale');
 
-  app.controller("SectionController", function($scope, $rootScope, $http, $stateParams) {
+  app.controller("SectionController", function($scope, $rootScope, $http, $stateParams, $sce) {
 
     $scope.menu1 = app.comune_informa;
     $scope.menu2 = app.cultura_turismo;
@@ -30,7 +45,7 @@
         }
         else if ((data.items[i].parent == root_parent) && (data.items[i+1].parent != root_parent))
         {
-          $scope.side_menu += '<li class="first"><a href="./#/section/' + $stateParams.menu + '/' + data.items[i].object + '/' + data.items[i].object_id + '">' + data.items[i].title + '</a>\n<ul>\n';
+          $scope.side_menu += '<li class="first" OnClick="show_submenu(this)"><a href="./#/section/' + $stateParams.menu + '/' + data.items[i].object + '/' + data.items[i].object_id + '">' + data.items[i].title + '</a>\n<ul>\n';
         }
         else if ((data.items[i].parent != root_parent) && (data.items[i+1].parent == root_parent))
         {
@@ -50,6 +65,8 @@
       {
         $scope.side_menu += '<li class="second"><a href="./#/section/' + $stateParams.menu + '/' + data.items[i].object + '/' + data.items[i].object_id + '">' + data.items[i].title + '</a></li>\n</ul>\n</li>\n';
       }
+      
+      $scope.side_menu = $sce.trustAsHtml($scope.side_menu);
       
     })
     .error(function(data, status, header, config) {
