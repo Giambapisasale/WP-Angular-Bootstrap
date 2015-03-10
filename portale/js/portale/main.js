da8f6e9fb53287df971e5f8c63b1684d6f71f07b
Helias <stefanoborzi32@gmail.com>
2015-03-10 15:19:24 +0100

    Aggiunto popup del login

--- portale/js/portale/main.js
+++ portale/js/portale/main.js
@@ -3,13 +3,52 @@
 
   app.controller("HomeController", function($scope, $stateParams, $http, $sce) {
     $http.get( app.wp + "posts/" )
-    .success(function(data, status, header, config) {
+      .success(function(data, status, header, config) {
       $scope.post = data;
       $scope.post.content = $sce.trustAsHtml(data.content);
     })
-    .error(function(data, status, header, config) {
+      .error(function(data, status, header, config) {
       console.log("Error in $http.get() of HomeController");
     });
+  });
+
+  app.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+    $scope.items = ['item1', 'item2', 'item3'];
+    $scope.open = function (size) {
+
+      var modalInstance = $modal.open({
+        template: '<iframe class="login" src="oauth/client.php"></iframe>',
+        controller: 'ModalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  });
+
+  app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
   });
 
 })()
