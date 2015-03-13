915b686dfdbb9fc95dfa189e1af12dd98e7f25b8
Helias <stefano520@hotmail.it>
2015-03-13 21:12:12 +0100

    Aggiunte icone checkbox, modificata lista dei servizi nell'admin panel

--- portale/js/portale/panel.js
+++ portale/js/portale/panel.js
@@ -6,6 +6,24 @@ function Main($scope) {
   var app = angular.module('portale');
 
   app.controller("PanelController", function($scope, $http) {
+    $http.get( app.wp )
+      .success(function(data, status, header, config) {
+      $scope.name = data.name;
+    })
+      .error(function(data, status, header, config) {
+      console.log("Error in $http.get() of PanelController (name)");
+    });
+
+    $http.get( app.wp + "menus/27" )
+      .success(function(data, status, header, config) {
+      $scope.panel_menu = data;
+      $scope.panel_menu.items.shift();
+    })
+      .error(function(data, status, header, config) {
+      console.log("Error in $http.get() of PanelController (panel_menu)");
+    });
+
+
     $http.get( "js/portale/contracts.json" )
       .success(function(data, status, header, config) {
       $scope.contracts_ = data;
