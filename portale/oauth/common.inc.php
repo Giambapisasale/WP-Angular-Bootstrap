02cec0e88c40d8e064101a1b3fa8a52db91ae6d3
Helias <stefano520@hotmail.it>
2015-03-14 13:10:40 +0100

    Fixato common

--- portale/oauth/common.inc.php
+++ portale/oauth/common.inc.php
@@ -5,13 +5,13 @@ require_once("OAuth.php");
  * Config Section
  */
 $domain = $_SERVER['HTTP_HOST'];
-$base = "/WP-Angular-Bootstrap/portale/oauth";
+$base = "/portale/oauth";
 
-//base_url indispensabile per il callback
+//base_url indispensabile per il callback	
 $base_url = "http://$domain$base";
 
 //da questo url vengono recuperati gli endpoint corretti
-$wp_json_url= "http://localhost/WP-Angular-Bootstrap/wordpress/wp-json/";
+$wp_json_url= "http://$domain/WP-Angular-Bootstrap/wordpress/wp-json/";
 
 
 // in caso di fallimento della prima request si potrebbero rigenerare?
