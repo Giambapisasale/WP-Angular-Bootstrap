0f649ada03e86171d2dd0a938798a4d61079e16d
Helias <stefano520@hotmail.it>
2015-03-14 12:49:40 +0100

    Ripristinati files oaut/*.php

--- portale/oauth/common.inc.php
+++ portale/oauth/common.inc.php
@@ -5,18 +5,18 @@ require_once("OAuth.php");
  * Config Section
  */
 $domain = $_SERVER['HTTP_HOST'];
-$base = "/portale/oauth";
+$base = "/WP-Angular-Bootstrap/portale/oauth";
 
 //base_url indispensabile per il callback
 $base_url = "http://$domain$base";
 
 //da questo url vengono recuperati gli endpoint corretti
-$wp_json_url= "http://$domain/wordpress/wp-json/";
+$wp_json_url= "http://localhost/WP-Angular-Bootstrap/wordpress/wp-json/";
 
 
 // in caso di fallimento della prima request si potrebbero rigenerare?
-$key = "sDmO3m2fls9V";
-$secret = "lVdn4090WgQqMpLOfivEDV04UpAEv3Y0NrRcmrSN2BeYpgaA";
+$key = "qg1qm8seOmwF";
+$secret = "F0fofyGe8X9kloOzrzE6xzktk35Ed2s7DHXfojVAiR7fWYoG";
 
 //signature utilizzata
 $hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
