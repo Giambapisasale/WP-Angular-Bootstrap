3b518ba270bd2301928fc71e3766c3f47af3629f
Giambattista Pisasale <giambapisasale@insolaria.it>
2015-03-14 12:22:12 +0100

    fix common

--- portale/oauth/common.inc.php
+++ portale/oauth/common.inc.php
@@ -1,6 +1,5 @@
 <?php
 require_once("OAuth.php");
-// require_once("../OAuth_TestServer.php");
 
 /*
  * Config Section
@@ -12,12 +11,12 @@ $base = "/portale/oauth";
 $base_url = "http://$domain$base";
 
 //da questo url vengono recuperati gli endpoint corretti
-$wp_json_url= "http://localhost/WP-Angular-Bootstrap/wordpress/wp-json/";
+$wp_json_url= "http://$domain/wordpress/wp-json/";
 
 
 // in caso di fallimento della prima request si potrebbero rigenerare?
-$key = "Z0ib5A99Ffd6";
-$secret = "Xu6VTDQmy0WjHFFLdjW0mPIy4CDFfYe1A9GSFtUBUr0KYPBA";
+$key = "sDmO3m2fls9V";
+$secret = "lVdn4090WgQqMpLOfivEDV04UpAEv3Y0NrRcmrSN2BeYpgaA";
 
 //signature utilizzata
 $hmac_method = new OAuthSignatureMethod_HMAC_SHA1();
