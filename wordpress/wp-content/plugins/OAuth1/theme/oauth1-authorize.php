f6e6034c4a578a981c1517606ffc4563119ea3c6
Helias <stefanoborzi32@gmail.com>
2015-03-15 23:05:24 +0100

    Aggiornato oauth1-authorize.php

--- wordpress/wp-content/plugins/OAuth1/theme/oauth1-authorize.php
+++ wordpress/wp-content/plugins/OAuth1/theme/oauth1-authorize.php
@@ -1,8 +1,8 @@
 <?php
 login_header(
-  __('Authorize', 'oauth'),
-  '',
-  $errors
+	__('Authorize', 'oauth'),
+	'',
+	$errors
 );
 
 $current_user = wp_get_current_user();
@@ -13,93 +13,90 @@ $url = site_url( 'wp-login.php?action=oauth1_authorize', 'login_post' );
 
 <style>
 
-  .login-title {
-    margin-bottom: 15px;
-  }
+	.login-title {
+		margin-bottom: 15px;
+	}
 
-  .login-info .avatar {
-    margin-right: 15px;
-    margin-bottom: 15px;
-    float: left;
-  }
+	.login-info .avatar {
+		margin-right: 15px;
+		margin-bottom: 15px;
+		float: left;
+	}
 
-  #login form .login-info p {
-    margin-bottom: 15px;
-  }
+	#login form .login-info p {
+		margin-bottom: 15px;
+	}
 
-  /** Note - login scope has not yet been implemented. **/
-  .login-scope {
-    clear: both;
-    margin-bottom: 15px;
-  }
+	/** Note - login scope has not yet been implemented. **/
+	.login-scope {
+		clear: both;
+		margin-bottom: 15px;
+	}
 
-  .login-scope h4 {
-    margin-bottom: 10px;
-  }
+	.login-scope h4 {
+		margin-bottom: 10px;
+	}
 
-  .login-scope ul {
-    margin-left: 1.5em;
-  }
+	.login-scope ul {
+		margin-left: 1.5em;
+	}
 
-  .submit {
-    clear: both;
-  }
+	.submit {
+		clear: both;
+	}
 
-  .submit .button {
-    margin-right: 10px;
-    float: left;
-  }
+	.submit .button {
+		margin-right: 10px;
+		float: left;
+	}
 
-  #oauth1_authorize_form {
-    margin: 0px;
-  }
+    #oauth1_authorize_form {
+      margin: 0px;
+    }
 </style>
 
 <form name="oauth1_authorize_form" id="oauth1_authorize_form" action="<?php echo esc_url( $url ); ?>" method="post">
 
-  <h2 class="login-title"><?php echo esc_html( sprintf( __('Connect %1$s'), $consumer->post_title ) ) ?></h2>
+	<h2 class="login-title"><?php echo esc_html( sprintf( __('Connect %1$s'), $consumer->post_title ) ) ?></h2>
 
-  <div class="login-info">
+	<div class="login-info">
 
-    <?php echo get_avatar( $current_user->ID, '78' ); ?>
+		<?php echo get_avatar( $current_user->ID, '78' ); ?>
 
-    <p><?php
-printf(
-  __( 'Vuoi connetterti a '.get_bloginfo( 'name' ).'?' )
-)
-      ?></p>
+		<p>Vuoi connetterti a <?= get_bloginfo( 'name' ) ?>?</p>
 
-  </div>
+	</div>
 
-  <?php
-  /**
+	<?php
+	/**
 	 * Fires inside the lostpassword <form> tags, before the hidden fields.
 	 *
 	 * @since 2.1.0
 	 */
-  do_action( 'oauth1_authorize_form', $consumer ); ?>
-  <p class="submit">
-    <button type="submit" name="wp-submit" value="Autorizza" class="button button-primary button-large"><?php _e('Authorize'); ?></button>
-    <button type="submit" name="wp-submit" value="cancel" class="button button-large" OnClick="parent.close_modal();"><?php _e('Cancel'); ?></button>
-  </p>
+	do_action( 'oauth1_authorize_form', $consumer ); ?>
+	<p class="submit">
+		<button type="submit" name="wp-submit" value="authorize" class="button button-primary button-large"><?php _e('Authorize'); ?></button>
+		<button type="submit" name="wp-submit" value="cancel" class="button button-large" OnClick="parent.close_modal();"><?php _e('Cancel'); ?></button>
+	</p>
 
 </form>
 
 <p id="nav">
-  <a href="<?php echo esc_url( wp_login_url( $url, true ) ); ?>"><?php _e( 'Switch user' ) ?></a>
-  <?php
-  if ( get_option( 'users_can_register' ) ) :
-  $registration_url = sprintf( '<a href="%s">%s</a>', esc_url( wp_registration_url() ), __( 'Register' ) );
-/**
+<a href="<?php echo esc_url( wp_login_url( $url, true ) ); ?>"><?php _e( 'Switch user' ) ?></a>
+<?php
+if ( get_option( 'users_can_register' ) ) :
+	$registration_url = sprintf( '<a href="%s">%s</a>', esc_url( wp_registration_url() ), __( 'Register' ) );
+	/**
 	 * Filter the registration URL below the login form.
 	 *
 	 * @since 1.5.0
 	 *
 	 * @param string $registration_url Registration URL.
 	 */
-echo ' | ' . apply_filters( 'register', $registration_url );
+	echo ' | ' . apply_filters( 'register', $registration_url );
 endif;
-  ?>
+?>
 </p>
 
 <?php
+login_footer();
