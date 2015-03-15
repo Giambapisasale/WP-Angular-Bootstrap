b52128c01f55e8a42b26fd33d6411654e169abee
Helias <stefano520@hotmail.it>
2015-03-15 11:18:04 +0100

    Modificata checkbox dell' admin panel (convertita in input radio)

--- portale/js/portale/panel.js
+++ portale/js/portale/panel.js
@@ -32,27 +32,6 @@ function Main($scope) {
       .error(function(data, status, header, config) {
       console.log("Error in $http.get() of PanelController (contracts)");
     });
-
-  });
-
-  app.directive('buttonToggle', function() {
-    return {
-      restrict: 'A',
-      require: 'ngModel',
-      link: function($scope, element, attr, ctrl) {
-        var classToToggle = attr.buttonToggle;
-        element.bind('click', function() {
-          var checked = ctrl.$viewValue;
-          $scope.$apply(function(scope) {
-            ctrl.$setViewValue(!checked);
-          });
-        });
-
-        $scope.$watch(attr.ngModel, function(newValue, oldValue) {
-          newValue ? element.addClass(classToToggle) : element.removeClass(classToToggle);
-        });
-      }
-    };
   });
 
 })()
